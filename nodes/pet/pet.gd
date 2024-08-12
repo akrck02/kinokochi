f95f0de9257c77a3af41b600508590b90e6f3f4f
akrck02 <aketza.vazquez@gmail.com>
2024-08-12 22:26:44 +0200

    new resources, stats and basic movement

--- nodes/pet/pet.gd
+++ nodes/pet/pet.gd
@@ -7,11 +7,31 @@ extends CharacterBody2D
 		pet_name = value
 		change_sprite()
 
-@onready var sprite = $Sprite
+@onready var sprite : Sprite2D = $Sprite
+@export var stats : PetStats
+
+const SPEED = 60
+
+func _physics_process(_delta):
+	var direction = Input.get_vector("ui_left","ui_right","ui_up","ui_down")
+	
+	print(direction)
+	
+	if direction:
+		self.velocity = direction * SPEED
+	else:
+		self.velocity = Vector2.ZERO
+		
+	move_and_slide()
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	change_sprite()
+	loadFromSavestate();
+	
+# loadPetDataFromSavestate
+func loadFromSavestate():
+	stats = PetStats.new();
 	
 # Change the sprite according to name
 func change_sprite():
--- nodes/pet/pet_stats.gd
+++ nodes/pet/pet_stats.gd
@@ -1,4 +1,12 @@
 class_name PetStats
+extends Node2D
+
+const DEFAULT_TIME = 0
+const DEFAULT_HUNGER = 0
+const DEFAULT_FUN = 50
+const DEFAULT_AFFECTION = 0
+const DEFAULT_ENERGY = 100
+const DEFAULT_EVOLUTION_STATE = 0
 
 var time : int
 var hunger : int
@@ -6,3 +14,11 @@ var fun : int
 var affection : int
 var energy : int
 var evolution_state : int
+
+func _init():
+	time = DEFAULT_TIME
+	hunger = DEFAULT_HUNGER
+	fun = DEFAULT_FUN
+	affection = DEFAULT_AFFECTION
+	energy = DEFAULT_ENERGY
+	evolution_state = DEFAULT_EVOLUTION_STATE
