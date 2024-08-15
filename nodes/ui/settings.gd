6fd3008add4690fd3ca3ac644ac3258ceb8d3a64
akrck02 <aketza.vazquez@gmail.com>
2024-08-15 20:11:19 +0200

    adjusting settings

--- nodes/ui/settings.gd
+++ nodes/ui/settings.gd
@@ -2,11 +2,11 @@ extends Control
 @onready var outline_check_button : Button = $Scroll/Margin/Controls/OutlineCheckButton
 @onready var exit_button : Button = $Scroll/Margin/Controls/ExitButton
 
-var outline : bool = true
+var outline : bool = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	outline_check_button.button_pressed=true
+	outline_check_button.button_pressed=false
 	outline_check_button.pressed.connect(toggle_outline)
 	exit_button.pressed.connect(exit)
 
