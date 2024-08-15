5a674c6e8e457b905ac5c5203f52fbd314431331
tekofx <tekofxx@gmail.com>
2024-08-15 18:24:01 +0200

    Implemented audio slider

--- nodes/ui/settings.gd
+++ nodes/ui/settings.gd
@@ -1,11 +1,13 @@
 extends Control
 @onready var control = $"../../.."
 @onready var outline_check_button = $"../OutlineCheckButton"
+@onready var volume_h_slider = $"../HBoxContainer/VolumeHSlider"
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	outline_check_button.button_pressed=SettingsManager.get_value("settings", "outline")
+	volume_h_slider.value=SettingsManager.get_value("settings", "volume")
 	pass # Replace with function body.
 
 
@@ -22,3 +24,8 @@ func toggle_outline():
 	var outline:bool=SettingsManager.get_value("settings", "outline")
 	SettingsManager.set_value("settings", "outline", !outline)
 	SignalDatabase.outline.emit(!outline)
+
+
+func set_volume(value:float):
+	var bus_index=AudioServer.get_bus_index("Master")
+	AudioServer.set_bus_volume_db(bus_index,linear_to_db(value))
--- shared/managers/settings_manager.gd
+++ shared/managers/settings_manager.gd
@@ -6,7 +6,7 @@ var response=config.load(settings_path)
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	if !FileAccess.file_exists(settings_path):
-		config.set_value("settings","volume", 100)
+		config.set_value("settings","volume", 1)
 		config.set_value("settings","outline", true)
 		config.save(settings_path)
 
