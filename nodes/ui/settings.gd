62e72d35bda8eeb11f412df0c12d4893a79a3ce1
tekofx <tekofxx@gmail.com>
2024-08-16 10:45:17 +0200

    Implemented audio slider

--- nodes/ui/settings.gd
+++ nodes/ui/settings.gd
@@ -1,14 +1,17 @@
 extends Control
 @onready var outline_check_button : Button = $Scroll/Margin/Controls/OutlineCheckButton
 @onready var exit_button : Button = $Scroll/Margin/Controls/ExitButton
+@onready var volume_h_slider = $Scroll/Margin/Controls/VolumeControls/VolumeHSlider
 
 var outline : bool = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	volume_h_slider.value=SettingsManager.get_value("settings", "volume")
 	outline_check_button.button_pressed=false
 	outline_check_button.pressed.connect(toggle_outline)
 	exit_button.pressed.connect(exit)
+	volume_h_slider.value_changed.connect(set_volume)
 
 func exit():
 	SignalDatabase.camera_movement_updated.emit(true);
@@ -17,3 +20,7 @@ func exit():
 func toggle_outline():
 	SignalDatabase.outline.emit(!outline)
 	outline=!outline
+
+func set_volume(value:float):
+	var bus_index=AudioServer.get_bus_index("Master")
+	AudioServer.set_bus_volume_db(bus_index,linear_to_db(value))
