244d945721281f23cf3354ade473e9726e84d7d2
tekofx <tekofxx@gmail.com>
2024-08-22 11:07:59 +0200

    Added x and y

--- scenes/liar/isometric_object.gd
+++ scenes/liar/isometric_object.gd
@@ -3,23 +3,30 @@ extends Node2D
 class_name IsometricObject
 
 ## Space between positions
-const length=40
+const length = 40
+@export var x:int
+@export var y:int
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	pass  # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
 
-func _cartesian_to_isometric(x:int,y:int):
-	var cartesian_pos=Vector2(x,y)
-	var isometric_pos=Vector2()
-	isometric_pos.x=(cartesian_pos.x-cartesian_pos.y)*length
-	isometric_pos.y=((cartesian_pos.x+cartesian_pos.y)/2)*length
+
+func _cartesian_to_isometric(x: int, y: int):
+	var cartesian_pos = Vector2(x, y)
+	var isometric_pos = Vector2()
+	isometric_pos.x = (cartesian_pos.x - cartesian_pos.y) * length
+	isometric_pos.y = ((cartesian_pos.x + cartesian_pos.y) / 2) * length
+	self.x=isometric_pos.x
+	self.y=isometric_pos.y
 	return isometric_pos
 
+
 ## Changes the object position but using isometric coordinates
-func move(x:int,y:int):
-	self.position=_cartesian_to_isometric(x,y)
+func move(x: int, y: int):
+	self.position = _cartesian_to_isometric(x, y)
