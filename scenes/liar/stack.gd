75990ffb81969cac3f9942cbd827fa21e9a4a4ff
tekofx <tekofxx@gmail.com>
2024-08-26 13:51:33 +0200

    Moved deck into stack

--- scenes/liar/stack.gd
+++ scenes/liar/stack.gd
@@ -1,9 +1,26 @@
 extends IsometricObject
 class_name Stack
+const PLAYERS = 4
+const CARDS_PER_HAND = 10
+## Array containing the cards
+var cards: Array
+enum COLORS_ENUM { red, yellow, green, blue }
+
+
+var card_scene=preload("res://scenes/liar/card.tscn")
 
 @onready var sprite_2d: Sprite2D = $Sprite2D
 
-var cards: Array
+var latest_added_cards: Array
+
+func _init() -> void:
+	self.cards = []
+	for color in COLORS_ENUM.keys():
+		for num in range(10):
+			var card=card_scene.instantiate()
+			card.color = color
+			card.number = num
+			cards.append(card)
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -16,6 +33,8 @@ func add_card(card:Card):
 		update_sprite()
 		
 func add_cards(cards:Array):
+	latest_added_cards=[]
+	latest_added_cards=cards
 	for card in cards:
 		add_card(card)
 		
@@ -33,3 +52,32 @@ func update_sprite():
 	sprite_2d.vframes=2
 	sprite_2d.texture = load("res://resources/sprites/cards/white.png")
 	
+
+func _to_string() -> String:
+	return str(cards)
+
+
+## Returns a random [Card] while removing it from the [Deck]
+func get_random_card() -> Card:
+	var random = randi() % cards.size()
+	return cards.pop_at(random)
+
+
+## Generates the hands of the game depending on the
+## number of [constant PLAYERS] and [constant CARDS_PER_HAND] [br]
+## Returns an [Array] containing [Hand]
+func generate_hands() -> Array:
+	var hands: Array = []
+	var stack = Stack.new()
+	var facing = 0
+	for player in PLAYERS:
+		var hand = Hand.new()
+		for c in CARDS_PER_HAND:
+			var card = stack.get_random_card()
+			card.facing = facing
+			hand.facing = facing
+			hand.add_card(card)
+		hands.append(hand)
+		facing += 1
+
+	return hands
